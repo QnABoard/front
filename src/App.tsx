3b11ae7582f42f6a4f22dbe1b2f684ee3343cd3f
HANTAEDONG <htd0913@gmail.com>
2024-12-08 16:39:49 +0900

    [Chore] react-qeury의 queryclient 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <>
-      <h1>리셋 css 적용</h1>
-    </>
-  );
-}
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // window 가 다시 포커스 될 때
+      refetchOnMount: false, // 쿼리의 새 인스턴스가 마운트 될 때
+      refetchOnReconnect: false, // 네트워크가 끊어졌다가 다시 연결될 때
+    },
+  },
+});
+
+const App = () => {
+  return <QueryClientProvider client={queryClient}>...</QueryClientProvider>;
+};
 
 export default App;
