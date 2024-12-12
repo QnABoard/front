b7dfb5b6ae37715f0fd8af62189a9f528c6fe86d
leechan <dlcks0601@gmail.com>
2024-12-12 16:11:24 +0900

    [Chore] MSW 개발 환경 초기화

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { worker } from './mocks/browser.ts';
 import App from './App.tsx';
 import '@/styles/reset.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+if (process.env.NODE_ENV === 'development') {
+  worker.start().then(() => {
+    createRoot(document.getElementById('root')!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  });
+} else {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
