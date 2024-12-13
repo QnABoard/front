3b792362cb31999eb5f318158cecec8f6e6e1cfd
HANTAEDONG <htd0913@gmail.com>
2024-12-13 14:29:56 +0900

    [Feat] Avator스타일과 스타일북 구현

---
+++ src/components/atoms/Avator.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+interface AvatarProps {
+  size?: 'big' | 'small';
+  disabled?: boolean;
+  src: string;
+  alt?: string;
+}
+
+const cx = (...classes: (string | boolean | undefined)[]) =>
+  classes.filter(Boolean).join(' ');
+
+const styles = {
+  base: `
+    display: inline-block;
+    border-radius: 50%;
+    overflow: hidden;
+    object-fit: cover; /* 이미지가 영역에 맞게 들어가도록 */
+  `,
+  big: `
+    width: 200px;
+    height: 200px;
+  `,
+  small: `
+    width: 64px;
+    height: 64px;
+  `,
+  disabled: `
+    opacity: 0.5;
+    pointer-events: none;
+  `,
+};
+
+const Avatar = styled.img.attrs<AvatarProps>(({ src, alt }) => ({
+  src,
+  alt: alt || 'Avatar',
+}))<AvatarProps>`
+  ${({ size, disabled }) =>
+    cx(
+      styles.base,
+      size === 'big' && styles.big,
+      size === 'small' && styles.small,
+      disabled && styles.disabled
+    )}
+`;
+
+export default Avatar;
