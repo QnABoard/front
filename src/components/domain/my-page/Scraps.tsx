c1fb515624e0b5338dafaecb3e69a51d913f0d93
HANTAEDONG <htd0913@gmail.com>
2024-12-13 16:57:07 +0900

    [Add] 마이페이지 하위 페이지 추가

---
+++ src/components/domain/my-page/Scraps.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Scraps = () => {
+  return <div>Scraps</div>;
+};
+
+export default Scraps;
---
+++ src/components/domain/my-page/UserContents.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserContentsPage = () => {
+  return <div>UserContentsPage</div>;
+};
+
+export default UserContentsPage;
