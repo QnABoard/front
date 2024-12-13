9c9a5fead9b96b1e77cd91995f0085994a99a1ec
HANTAEDONG <htd0913@gmail.com>
2024-12-13 14:38:56 +0900

    [Feat] 마이페이지 라우팅

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,14 @@
 import HomePage from '@/pages/HomePage';
+import MyPage from '@/pages/MyPage';
 
 const router = [
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/mypage/:id',
+    element: <MyPage />,
   },
 ];
 
