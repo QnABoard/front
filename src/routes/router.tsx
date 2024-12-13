912bba5ef9154780f8b2096c6c51431179a3605a
leechan <dlcks0601@gmail.com>
2024-12-13 16:57:59 +0900

    [Feat] 회원가입, 로그인 페이지 라우팅 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,19 @@
 import HomePage from '@/pages/HomePage';
+import JoinPage from '@/pages/JoinPage';
+import LoginPage from '@/pages/LoginPage';
 
 const router = [
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/join',
+    element: <JoinPage />,
   },
 ];
 
