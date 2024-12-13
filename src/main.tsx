4acbc7c8326acfb615a69cebc18d917f89cc6f24
HANTAEDONG <htd0913@gmail.com>
2024-12-13 14:09:33 +0900

    [Fix] msw 오류해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { worker } from './mocks/browser.ts';
 import App from './App.tsx';
 import '@/styles/reset.css';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start().then(() => {
-    createRoot(document.getElementById('root')!).render(
-      <StrictMode>
-        <App />
-      </StrictMode>
-    );
-  });
-} else {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start().then(() => {
+//     createRoot(document.getElementById('root')!).render(
+//       <StrictMode>
+//         <App />
+//       </StrictMode>
+//     );
+//   });
+// } else {
+//   createRoot(document.getElementById('root')!).render(
+//     <StrictMode>
+//       <App />
+//     </StrictMode>
+//   );
+// }
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
