7ad4f9d845299218c532ee613a0ae2ce4995957f
leechan <dlcks0601@gmail.com>
2024-12-17 02:44:07 +0900

    [Refactor] 필요없는 타입 삭제

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -13,16 +13,6 @@ export interface UserState {
   error: string | null;
 }
 
-// 로그인 요청 데이터 타입
-export interface LoginRequest {
-  email: string;
-  password: string;
-}
-
-export interface APIError {
-  message: string;
-}
-
 export interface DecodedToken {
   id: number;
   email: string;
@@ -30,16 +20,4 @@ export interface DecodedToken {
   role: string;
   iat: number;
   exp: number;
-}
-
-// Thunk 반환 타입
-export interface LoginThunkResponse {
-  token: string; // JWT 토큰
-  decodedToken: DecodedToken; // 디코딩된 데이터
-}
-
-export interface LoginResponse {
-  message: any;
-  success: boolean;
-  token?: string;
 }
