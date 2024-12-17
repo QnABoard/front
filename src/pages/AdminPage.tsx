c62a43ab80ae8b48358d79c0c0857b6fd9fa829b
leechan <dlcks0601@gmail.com>
2024-12-18 01:25:06 +0900

    [Feat] 어드민 페이지 생성

---
+++ src/pages/AdminPage.tsx
@@ -0,0 +1,10 @@
+const AdminPage = () => {
+  return (
+    <div className='p-8'>
+      <h1 className='text-2xl font-bold'>어드민 페이지</h1>
+      <p>이 페이지는 관리자만 접근할 수 있습니다.</p>
+    </div>
+  );
+};
+
+export default AdminPage;
