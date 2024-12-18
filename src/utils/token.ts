ac2c7102aafbd6184b673bf7a3777f17bad47146
leechan <dlcks0601@gmail.com>
2024-12-18 14:16:39 +0900

    [Refactor] const -> function 의미있는 변화 x

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,12 +1,11 @@
-// 로컬스토리지에서 토큰 가져오기
-export const getToken = (): string | null => {
+export function getToken(): string | null {
   return localStorage.getItem('token');
-};
+}
 
-export const setToken = (token: string) => {
+export function setToken(token: string) {
   localStorage.setItem('token', token);
-};
+}
 
-export const removeToken = () => {
+export function removeToken() {
   localStorage.removeItem('token');
-};
+}
