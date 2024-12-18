487eda4b73fd59ed9adc61dd3978d40618bb5f20
leechan <dlcks0601@gmail.com>
2024-12-18 14:13:45 +0900

    [Refactor] 필요없는 부분 제거, BASE_URL export

--- src/apis/http.api.ts
+++ src/apis/http.api.ts
@@ -1,20 +1,9 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+import { getToken, removeToken } from '@/utils/token';
+
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const DEFAULT_TIMEOUT = 30000; // 요청 제한 시간
-
-// 토큰 관리 함수
-function getToken(): string | null {
-  return localStorage.getItem('token');
-}
-
-function setToken(token: string) {
-  localStorage.setItem('token', token);
-}
-
-function removeToken() {
-  localStorage.removeItem('token');
-}
 
 export const createClient = (config?: AxiosRequestConfig) => {
   const token = getToken(); // 토큰 가져오기
@@ -61,35 +50,3 @@ export const createClient = (config?: AxiosRequestConfig) => {
 };
 
 export const httpClient = createClient();
-
-// 토큰 관련 함수 export
-export { setToken, removeToken, getToken };
-
-// 공통 요청 부분
-
-type RequestMethod = 'get' | 'post' | 'put' | 'delete';
-
-export const requestHandler = async <R = undefined, T = undefined>(
-  method: RequestMethod,
-  url: string,
-  payload?: T
-) => {
-  let response;
-
-  switch (method) {
-    case 'post':
-      response = await httpClient.post<R>(url, payload);
-      break;
-    case 'get':
-      response = await httpClient.get<R>(url);
-      break;
-    case 'put':
-      response = await httpClient.put<R>(url, payload);
-      break;
-    case 'delete':
-      response = await httpClient.delete<R>(url);
-      break;
-  }
-
-  return response.data;
-};
