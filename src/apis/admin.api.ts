f96fe9599c4d6c30809f678324dad8d2f30a78b5
leechan <dlcks0601@gmail.com>
2024-12-19 17:00:18 +0900

    [Refactor] 유저 탈퇴 api 연결

--- src/apis/admin.api.ts
+++ src/apis/admin.api.ts
@@ -11,3 +11,16 @@ export const fetchAdminData = async () => {
     throw error;
   }
 };
+
+export const deleteUser = async (id: number): Promise<{ success: boolean }> => {
+  try {
+    const response = await httpClient.delete(`/api/users/${id}/delete`);
+    console.log('응답 데이터:', response.data);
+    return response.data;
+  } catch (error: any) {
+    console.error('API 호출 실패:', error);
+    throw new Error(
+      error.response?.data?.message || '회원 탈퇴 중 오류가 발생했습니다.'
+    );
+  }
+};
