17d87ef83a1c15bf8ff0c88ce5904e9ae6b671ca
HongbiKo <rhghdql666@gmail.com>
2024-12-20 13:45:35 +0900

    [Chore] 게시물리스트 posts 요청 usequery에서 요청한 데이터 받아오도록 수정

--- src/components/main-page/QuestionBox.tsx
+++ src/components/main-page/QuestionBox.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import QuestionBody from '../ui/molecules/mainpage-molecule/QuestionBody';
 import QuestionHeader from '../ui/molecules/mainpage-molecule/QuestionHeader';
 import QuestionTag from '../ui/atoms/mainpage-atom/QuesitonTag';
 import QuestionBottom from '../ui/molecules/mainpage-molecule/QuestionBottom';
-import { fetchMainData } from '@/apis/maindata.api';
 import { mainPosts } from '@/types/main.model';
 
 const QuestionBoxContainer = styled.div`
@@ -18,32 +16,38 @@ const QuestionItem = styled.div`
   margin: 10px;
 `;
 
-function QuestionBox() {
-  const [posts, setPosts] = useState<mainPosts[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+interface Props {
+  posts?: mainPosts[];
+  isLoading: boolean;
+  error?: unknown;
+}
 
-  useEffect(() => {
-    const loadPosts = async () => {
-      try {
-        const data = await fetchMainData();
-        setPosts(data.posts);
-      } catch (err) {
-        setError('데이터를 불러오는 중 오류가 발생했습니다.');
-      } finally {
-        setLoading(false);
-      }
-    };
+function QuestionBox({posts = [], isLoading, error} : Props) {
+  // const [posts, setPosts] = useState<mainPosts[]>([]);
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const [error, setError] = useState<string | null>(null);
 
-    loadPosts();
-  }, []);
+  // useEffect(() => {
+  //   const loadPosts = async () => {
+  //     try {
+  //       const data = await fetchMainData();
+  //       setPosts(data.posts);
+  //     } catch (err) {
+  //       setError('데이터를 불러오는 중 오류가 발생했습니다.');
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-  if (loading) return <div>데이터를 불러오는 중...</div>;
-  if (error) return <div>{error}</div>;
+  //   loadPosts();
+  // }, []);
+
+  if (isLoading) return <div>데이터를 불러오는 중...</div>;
+  if (error as boolean) return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
 
   return (
     <QuestionBoxContainer>
-      {posts.map((post) => (
+      {Array.isArray(posts) && posts.map((post) => (
         <QuestionItem key={post.id}>
           <QuestionHeader solved={post.solved} title={post.title} />
           <QuestionBody content={post.content} />
