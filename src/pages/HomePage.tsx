e8ec8ee7e9ad8b0f92e754bf1b1d9cb9e563720b
HongbiKo <rhghdql666@gmail.com>
2024-12-21 16:17:28 +0900

    Squashed commit of the following:
    commit 4f02a2d9aa5c586b9187cd06aede88865eac7438
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 22:01:13 2024 +0900
    
        [Refactor] fetch 파일로 분리
    
    commit 59b95d14037d11f954fcb2b87b96e3285380df0c
    Merge: 0eed3c6 1901dd6
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 17:58:08 2024 +0900
    
        Merge branch 'develop' into feature/22-메인페이지-태그리스트,페이지네이션,게시물
    
    commit 0eed3c6662290deb81305e278c6177cad7e27673
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 13:46:53 2024 +0900
    
        [Chore] 메인페이지 수정
    
    commit c832f3f2d529ea94cd5eb2fb8a0a71536fcbe302
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 13:46:23 2024 +0900
    
        [Chore] 메인태그리스트컴포넌트 수정
    
    commit 17d87ef83a1c15bf8ff0c88ce5904e9ae6b671ca
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 13:45:35 2024 +0900
    
        [Chore] 게시물리스트 posts 요청 usequery에서 요청한 데이터 받아오도록 수정
    
    commit 6af12c622d977215a328e1fdbc6f27b17ce9208c
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 13:43:27 2024 +0900
    
        [Feat] pagination 타입 정의 및 컴포넌트 추가
    
    commit 7afea101239873de3ce8016c9fd2040a1c738451
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 13:42:14 2024 +0900
    
        [Feat] usequery 사용해서 기본데이터 요청 메서드 정의
    
    commit b90d193516fb45d7b44ded77b2655d7f6fcd58b3
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Fri Dec 20 13:41:01 2024 +0900
    
        [Delete] 필요없는 파일 삭제
    
    commit fad03d115f2525c2662e6f0a536596d1c4f47c04
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Thu Dec 19 23:56:13 2024 +0900
    
        [Chore] 파일제거 및 메인페이지타입 수정
    
    commit cf2cd1d179aaa2da70be16e614c1047657723ab9
    Author: HongbiKo <rhghdql666@gmail.com>
    Date:   Thu Dec 19 17:30:37 2024 +0900
    
        [Style] 메인페이지 태그리스트, 검색바 디자인 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,99 @@
-import MainTagList from '@/components/main-page/MainTagList';
-import SearchInput from '@/components/main-page/SearchInput';
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import QuestionBox from '@/components/main-page/QuestionBox';
+import TagList from '@/components/main-page/TagList';
+import { useNavigate } from 'react-router';
+import { fetchMainData } from '@/apis/main.api';
+import SearchInput from '@/components/main-page/SearchInput';
+import Pagination from '@/components/main-page/Pagination';
+import { Pagination as IPagination} from '@/types/pagination.model';
+import { mainTags } from '@/types/main.model';
 
 const HomePage = () => {
+  const [selectedTags, setSelectedTags] = useState<number[]>([]);
+  const [hometags, setHomeTags] = useState<mainTags[]>([]);
+  const [homepagination, setHomePagination] = useState<IPagination>();
+  const navigate = useNavigate();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['mainData', selectedTags],
+    queryFn: () =>
+      fetchMainData({
+        selectedTags,
+        currentPage: 1,
+        limit: 20,
+      }),
+    enabled: true, // 데이터 fetch 실행!!!
+  });
+
+  const { posts, tags:fetchedTags, pagination:fetchedPagination, filteredPosts } = data ?? {};
+
+  // 태그, 페이지네이션 상태로 관리
+  useEffect(() => {
+    if (fetchedTags) {
+      setHomeTags(fetchedTags);
+    }
+    if (fetchedPagination) {
+      setHomePagination(fetchedPagination);
+    }
+  }, [fetchedTags, fetchedPagination]);
+
+  // url 달라질때 태그들 상태도 달라지게 요청
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const tagsFromURL = params.getAll('tags').map((tag) => Number(tag));
+    setSelectedTags(tagsFromURL);
+
+  }, [location.search]);
+
+  // 새로고침 시 '/'로 이동 (전체 데이터 뿌림)
+  useEffect(() => {
+    if (location.search) {
+      navigate('/', { replace: true });
+    }
+  }, []);
+
+
+  // 태그 도글
+  const toggleTag = (tagId: number) => {
+    setSelectedTags((prev) => {
+      const updatedTags = prev.includes(tagId)
+      ? prev.filter((id) => id !== tagId) // 이미 선택된 태그를 클릭하면 제거
+      : [...prev, tagId]; // 새 태그를 클릭하면 추가
+
+      //선택된 태그에 따라 URL 쿼리 파라미터 업데이트
+      if (updatedTags.length > 0) {
+        navigate({
+          search: `?tags=${updatedTags.join('&tags=')}`, 
+        });
+      } else {
+        navigate({
+          search: '', 
+        });
+      }
+
+      return updatedTags;
+    });
+  };
+
+  // 필터링된 게시글을 가져오고, 없으면 기본 게시글 사용
+  const postsToDisplay = selectedTags.length > 0 ? filteredPosts : posts;
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) {
+    return <p>Error occurred: {error instanceof Error ? error.message : 'An unknown error occurred'}</p>;
+  }
+
   return (
     <div>
       <SearchInput />
-      <MainTagList />
-      <QuestionBox />
+      {
+        hometags && (
+          <TagList tags={hometags} selectedTags={selectedTags} onTagToggle={toggleTag} />
+        )
+      }
+      <QuestionBox posts={postsToDisplay || []} isLoading={isLoading} />
+      <Pagination pagination={homepagination}/>
     </div>
   );
 };
